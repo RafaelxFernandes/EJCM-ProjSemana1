7b876c5ea60718c3b8555225d03e190bd6b8623c
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2018-07-19 23:22:28 -0300

    Versao 0.3 - Completar components!

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ArduinoHomeComponent } from './arduino-home/arduino-home.component';
-
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { ArduinoHomeComponent } from './arduino-home/arduino-home.component';
+import { ArduinoCatalogoComponent } from './arduino-catalogo/arduino-catalogo.component';
+import { ArduinoContatoComponent } from './arduino-contato/arduino-contato.component';
+import { ArduinoSobrenosComponent } from './arduino-sobrenos/arduino-sobrenos.component';
+import { ArduinoLoginComponent } from './arduino-login/arduino-login.component';
+
 const appRoutes: Routes = [
 	{ path: 'home', component: ArduinoHomeComponent },
-	{ path: '', redirectTo: '/home', pathMatch: 'full' }
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: 'catalogo', component: ArduinoCatalogoComponent },
+	{ path: 'contato', component: ArduinoContatoComponent },
+	{ path: 'sobrenos', component: ArduinoSobrenosComponent },
+	{ path: 'login', component: ArduinoLoginComponent }
 ];
 
 @NgModule({
